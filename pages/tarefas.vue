0984ebc26f5c39fed3dde9824ec0b4a0c3482aeb
cristianebeel <cristianebeel@live.com>
2021-05-11 16:05:24 -0300

    Adicionando Socket.IO

--- pages/tarefas.vue
+++ pages/tarefas.vue
@@ -16,7 +16,7 @@
                 class="mt-6 w-full max-w-screen-xl border border-gray-500
                     rounded-lg p-4 divide-y divide-gray-300 bg-gray-100"
             >
-                <li v-for="task of taskList"
+                <li v-for="task of sortedTasks"
                     :key="task._id"
                     class="flex items-center px-2 py-1 cursor-pointer hover:bg-blue-200"
                     @click.left="toggleTask(task)"
@@ -34,24 +34,39 @@
 </template>
 
 <script>
+import io from 'socket.io-client'
+
 export default {
     data(){
         return{
+            waiting: false,
             inputText: '',
-            taskList: [],
-            waiting: false
+            taskList: []
         }
+    },
+
+    mounted(){
+        const socket = io('http://localhost:8080')
+
+        socket.on('todo-created', created => {
+            this.taskList.push(created)
+        })
+
+        socket.on('todo-updated', update => {
+            const task = this.taskList.find(t => t._id === updated._id)
+            task.done = updated.done
+        })
+
+        socket.on('todo-removed', removed =>{
+            this.taskList = this.taskList.filter(t => t._id !== removed._id)
+        })
     },
 
     async fetch(){
         try{
             this.waiting = true
             this.taskList = await this.$axios.$get('http://localhost:8080/todos')
-            this.taskList.forEach(i => i.createdAt = new Date(i.createdAt))
-            for(let i of this.taskList){
-                // i.createdAt = i.createdAt.substr(0,10) pega somente a data
-                i.createdAt = new Date(i.createdAt) //transforma em dara pq o axios salva como string
-            }
+            this.taskList.forEach(t => t.createdAt = new Date(t.createdAt))
         }catch{
             alert('Ops! Parece que deu erro...')
         }finally{
@@ -61,52 +76,57 @@ export default {
 
     filters: {
         dateToString(value){
-            //return value.toISOString()
-            return value.toLocaleString('pt-BR')
+            if(typeof value === 'string') value = new Date(value)
+                return value.toLocaleString('pt-BR')
+        }
+    },
+    
+    computed: {
+        sortedTasks(){
+            // Ordem alfabética
+            //return this.taskList.sort((a, b) => a.text.localeCompare(b.text))
+
+            //Ordem de data de criação
+            return this.taskList.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
         }
     },
 
     methods: {
         async addTask(){
             try{
-                /*await fetch({
-                    url: 'http://localhost:8080/todos',
-                    method: 'POST',
-                    body: {
-                        text: this.inputText,
-                        createdAt: new Date()
-                    }
-                })*/
-
-                const task = {
-                    text: this.inputText,
-                    createdAt: new Date()
-                }
+                this.waiting = true
+                const task = { text: this.inputText }
 
                 await this.$axios.$post('http://localhost:8080/todos', task)
-                
-                this.$fetch()
                 this.inputText = ''
             }
             catch{
                 alert('Erro ao criar tarefa.')
+            }finally{
+                this.waiting = false
             }
         },
 
         async toggleTask(task){
-            await this.$axios.$put(`http://localhost:8080/todos/${task._id}`, {
-                text: task.text,
-                createdAt: task.createdAt,
-                updatedAt: new Date(),
-                done: !task.done
-            })
-            this.$fetch()
+            try{
+                this.waiting = true
+                await this.$axios.$put(`http://localhost:8080/todos/${task._id}`, {
+                    text: task.text,
+                    createdAt: task.createdAt,
+                    updatedAt: new Date(),
+                    done: !task.done
+                })
+            }catch{
+                alert('Erro ao atualizar')
+            }finally{
+                this.waiting = false
+            }
         },
 
         async deleteTask(task){
             try{
+                this.waiting = true
                 await this.$axios.$delete(`http://localhost:8080/todos/${task._id}`)
-                this.$fetch()
             }catch{
                 alert('Erro na exclusão')
             }finally{
